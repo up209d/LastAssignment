a961796bdfdf7b8579b3569ad74a3089621ae6d6
up209d <up209d@gmail.com>
2016-04-25 03:38:18 +1000

    Changes

--- project/index.js
+++ project/index.js
@@ -1,18 +1,27 @@
 var viewPort = document.getElementById('view-port');
 var cloudContainer = document.getElementById('cloud-way');
 
-var renderer = PIXI.autoDetectRenderer(cloudContainer.width,cloudContainer.height,{view: cloudContainer, backgroundColor : 0x1099bb });
+var renderer = PIXI.autoDetectRenderer(cloudContainer.width,cloudContainer.height,{view: cloudContainer, transparent:false, backgroundColor : 0x1099bb });
 
 var stage = new PIXI.Container();
+
+/* call this function whenever you added a new layer/container */
+stage.updateLayersOrder = function () {
+    stage.children.sort(function(a,b) {
+        a.zIndex = a.zIndex || 0;
+        b.zIndex = b.zIndex || 0;
+        return b.zIndex - a.zIndex
+    });
+};
 
 var cloudTextures = ['assets/images/cloud.png','assets/images/cloud2.png'];
 
 
 // Clouds object
 
-var clouds = {};
+var clouds = [];
 var cloudIndex = 0;
-var cloudCount = 5;
+var cloudCount = 1;
 var frameCount = 0;
 
 function Cloud() {
@@ -23,21 +32,23 @@ function Cloud() {
     //Init the transform of cloud
 
     this.object.anchor.x = 0.5;
-    this.object.anchor.y = 0.0;
+    this.object.anchor.y = 0;
 
-    this.object.position.x = Math.random()*200+400;
-    this.object.position.y = Math.random()*200+400;
+    this.object.position.x = Math.random()*800;
+    this.object.position.y = 300;
     
     this.object.velocityX = 0;
-    this.object.velocityY = 0.1;
+    this.object.velocityY = 0.01;
 
-    this.object.scale.x = 1;
-    this.object.scale.y = 1;
+    this.object.scale.x = 0.5;
+    this.object.scale.y = 0.5;
 
     this.object.alpha = 0.0;
     
     this.object.life= 0;
-    this.object.maxLife = Math.random()*100+100;
+    this.object.maxLife = 200;
+    this.object.dieing= false;
+    
     
     // Add to object collection
     clouds[cloudIndex] = this;
@@ -47,6 +58,8 @@ function Cloud() {
     
     stage.addChild(this.object);
     
+    stage.children.sort(function(a, b){return b.id - a.id});
+    
 }
 
 Cloud.prototype.move = function() {
@@ -55,9 +68,9 @@ Cloud.prototype.move = function() {
     this.object.life++;
     
     if (this.object.life >= this.object.maxLife) {
-        stage.removeChild(this.object);
-        delete clouds[this.object.id];
-        delete this;
+//        stage.removeChild(this.object);
+//        delete clouds[this.object.id];
+//        delete this;
     }
     
     
@@ -69,14 +82,41 @@ Cloud.prototype.move = function() {
     this.object.scale.x += 0.01;
     this.object.scale.y += 0.01;
     
-    if (this.object.alpha < 0.9) {
-        this.object.alpha += 0.08;        
+    if (this.object.dieing) {
+        
+            //Check Life
+            this.object.life++;
+
+            if (this.object.life >= this.object.maxLife) {
+                
+                this.object.alpha -= 0.05;
+
+                if (this.object.alpha < 0) {
+
+                    stage.removeChild(this.object);
+                    delete clouds[this.object.id];
+                    delete this;
+
+
+                }
+                
+            }
+        
+
+        
+    } else {
+        
+        this.object.alpha += 0.1;
+        
+        if (this.object.alpha > 0.9) {
+            
+             this.object.dieing = true;
+            
+        }
+
+        
     }
-    
 
-
-    
-    
 }
 
 
@@ -86,7 +126,7 @@ function animate() {
     requestAnimationFrame(animate);
     frameCount++;
     
-    if ((frameCount%30) == 0) {
+    if ((frameCount%5) == 0) {
         
         for (var i=0;i<cloudCount;i++) {
             new Cloud();        
